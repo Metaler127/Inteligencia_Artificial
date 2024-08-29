ff9d767e947afbfb81a4e768bf4a169d84a21092
Metaler127 <ccarballo_21@alu.uabcs.mx>
2024-08-28 21:53:30 -0700

    Area de un circulo

---
+++ main.py
@@ -0,0 +1,8 @@
+radio = float(input("Ingresa el radio del círculo: "))
+
+def area(radio):
+    pi = 3.1416
+    return pi * radio * radio
+
+resultado = area(radio)
+print("El área de tu círculo es:", resultado)
